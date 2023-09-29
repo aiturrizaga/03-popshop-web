6422946b3d354110b9c3a5e90846dae0aac5d9c6
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2023-09-29 13:04:38 -0500

    Servicios

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['./category-list.component.css'],
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  categories: string[] = [];
 
+  constructor(private categoryService: CategoryService) {}
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.categoryService.findAll().subscribe((res) => (this.categories = res));
+  }
 }
